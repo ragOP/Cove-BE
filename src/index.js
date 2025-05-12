ad836dc071b17e1b6c9899312695ca0f8c2cdf51
farish-jamal <farishjamal98@gmail.com>
2025-05-12 23:26:59 +0530

    Refactor environment variable handling to include NODE_ENV and improve error logging

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ const http = require('http');
 const app = require('./app');
 const { connectToDatabase } = require('./config/db');
 
-const { PORT, MONGODB_URI } = process.env;
+const { MONGODB_URI } = process.env;
+
+const { NODE_ENV } = process.env;
+const PORT = process.env.PORT || 5000;
 
 // Check if required environment variables are set
-if (!PORT || !MONGODB_URI) {
-  console.error('Missing required environment variables: PORT, MONGODB_URI', {
-    PORT,
+if (!MONGODB_URI || !NODE_ENV || !PORT) {
+  console.error('Missing required environment variables: MONGODB_URI, NODE_ENV, PORT', {
     MONGODB_URI,
+    NODE_ENV,
+    PORT,
   });
   process.exit(1);
 }
@@ -21,5 +25,5 @@ connectToDatabase(MONGODB_URI);
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT} in ${NODE_ENV} mode`);
 });
