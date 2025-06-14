5c6f49bdf578c4250d3d6d2666d3e4ad4edfcd8a
farish-jamal <farishjamal98@gmail.com>
2025-06-14 18:09:11 +0530

    Refactor private message handling by moving logic to socket utility module. Implement chat list updates for both sender and receiver, including unread message counts and participant details, enhancing real-time messaging functionality.

--- src/utils/socket/index.js
+++ src/utils/socket/index.js
@@ -1,5 +1,7 @@
-const { getIO } = require("../../config/socket");
-const User = require("../../models/userModel");
+const { getIO } = require('../../config/socket');
+const User = require('../../models/userModel');
+const OneToOneChat = require('../../models/chatModel');
+const messageModel = require('../../models/messageModel');
 
 const emittedMessages = new Set();
 
@@ -18,8 +20,89 @@ exports.emitNewMessage = async (message, chat, receiverId) => {
       chat: chat._id,
     });
     emittedMessages.add(messageId);
-    setTimeout(() => {
-      emittedMessages.delete(messageId);
-    }, 60 * 1000);
+    const receiverChats = await OneToOneChat.find({ participants: receiverId })
+      .populate('participants', 'name username profilePicture')
+      .populate({
+        path: 'lastMessage',
+        populate: {
+          path: 'sender',
+          select: 'name username profilePicture',
+        },
+      })
+      .sort({
+        lastMessage: -1,
+      });
+
+    const receiverChatResults = await Promise.all(
+      receiverChats.map(async chat => {
+        const unreadCount = await messageModel.countDocuments({
+          chat: chat._id,
+          receiver: receiverId,
+          status: 'sent',
+        });
+        const otherParticipant = chat.participants.filter(
+          p => p._id.toString() !== receiverId.toString()
+        );
+        const isFriend = await User.findById(receiverId).then(user =>
+          user.friends.includes(otherParticipant[0]._id)
+        );
+        return {
+          ...chat.toObject(),
+          lastMessage: chat.lastMessage,
+          unreadCount,
+          isFriend,
+          chatWith: otherParticipant,
+        };
+      })
+    );
+
+    io.to(receiver.socketId).emit('chat_list_update', {
+      success: true,
+      data: receiverChatResults,
+    });
   }
+
+  const senderChats = await OneToOneChat.find({ participants: socket.user._id })
+    .populate('participants', 'name username profilePicture')
+    .populate({
+      path: 'lastMessage',
+      populate: {
+        path: 'sender',
+        select: 'name username profilePicture',
+      },
+    })
+    .sort({
+      lastMessage: -1,
+    });
+
+  const senderChatResults = await Promise.all(
+    senderChats.map(async chat => {
+      const unreadCount = await messageModel.countDocuments({
+        chat: chat._id,
+        receiver: socket.user._id,
+        status: 'sent',
+      });
+      const otherParticipant = chat.participants.filter(
+        p => p._id.toString() !== socket.user._id.toString()
+      );
+      const isFriend = await User.findById(socket.user._id).then(user =>
+        user.friends.includes(otherParticipant[0]._id)
+      );
+      return {
+        ...chat.toObject(),
+        lastMessage: chat.lastMessage,
+        unreadCount,
+        isFriend,
+        chatWith: otherParticipant,
+      };
+    })
+  );
+
+  socket.emit('chat_list_update', {
+    success: true,
+    data: senderChatResults,
+  });
+  setTimeout(() => {
+    emittedMessages.delete(messageId);
+  }, 60 * 1000);
 };
